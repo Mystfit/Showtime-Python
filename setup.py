d28425845c119a3c44b1b45927b3038107e2cdf9
Mystfit <byronated@gmail.com>
2014-08-18 18:38:27 +1200

    Moved example scripts around and made the stage script executable. Updated setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,38 @@
+import os
 from setuptools import setup, find_packages
 
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(name='Showtime-Python',
-      version='1.0',
-      description='The ZstShowtime library is designed for live performances using ZeroMQ.',
-      author='Byron Mallett',
+      version='1.0.2',
+      description='Showtime-Python allows you to connect multiple programs together for live performances using nodes.',
+      long_description=read('README.md'),
       author_email='byronated@gmail.com',
       url='http://github.com/Mystfit/Showtime',
       license='MIT',
-      install_requires=["pyzmq"],
+      install_requires=["pyzmq==14.3.1"],
       packages=find_packages(),
-)
+      classifiers=[
+      'Development Status :: 4 - Beta',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: Microsoft :: Windows :: Windows 7',
+      'Operating System :: MacOS :: MacOS X',
+      'Operating System :: OS Independent',
+      'Environment :: Console',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3.3',
+      'Programming Language :: C#',
+      'Programming Language :: Java',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'Topic :: Software Development :: Object Brokering'
+      'Intended Audience :: Developers',
+	  'Natural Language :: English'
+      ],
+      scripts=['scripts/showtime-stage.py']
+      )
