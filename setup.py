c7dcb3c28b18e192a2dc8ab6e4d9f0e49617646b
Byron Mallett <byronated@gmail.com>
2014-08-21 21:28:59 +1200

    Updated readme and version number

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 import os
 from setuptools import setup, find_packages
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+readme = open('README.rst', 'r')
+README_TXT = readme.read()
 
 setup(name='Showtime-Python',
-      version='1.0.2',
-      description='Showtime-Python allows you to connect multiple programs together for live performances using nodes.',
-      long_description=read('README.md'),
+      version='1.0.3',
+      description='Showtime-Python allows you to connect multiple programs for live performances using nodes.',
+      long_description=README_TXT,
       author='Byron Mallett',
       author_email='byronated@gmail.com',
       url='http://github.com/Mystfit/Showtime',
@@ -33,7 +29,7 @@ setup(name='Showtime-Python',
       'Topic :: Software Development :: Libraries :: Python Modules',
       'Topic :: Software Development :: Object Brokering',
       'Intended Audience :: Developers',
-	  'Natural Language :: English'
+        'Natural Language :: English'
       ],
       scripts=['scripts/showtime-stage.py']
       )
